04ba1ce63f97bbf549d175140711b60c70888450
Thragull <146970246+Thragull@users.noreply.github.com>
2024-02-14 18:54:11 +0000

    settings iniciales

---
+++ src/js/component/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import List from "./List";
+
+//include images into your bundle
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="text-center">
+			<List />
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/js/component/List.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+let thingsToDo = [];
+
+const List = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default List
